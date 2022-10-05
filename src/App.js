7910263ef5d4e304398e36b13940f77c0e8b8132
Devers <deverstalmage@gmail.com>
2022-10-04 21:55:01 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import GameState from './GameState.js';
 
 function App() {
+  const [deck, setDeck] = useState();
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Card game
       </header>
+      <GameState>
+        <p>testing...</p>
+      </GameState>
     </div>
   );
 }
---
+++ src/GameState.js
@@ -0,0 +1,5 @@
+export default function GameState({ children }) {
+  return (
+    { children }
+  );
+}
